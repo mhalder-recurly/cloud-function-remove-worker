77db63ee7925bdb73d972cf7b0b20151ab918c59
Matt Halder <mhalder@recurly.com>
2021-03-31 15:07:49 -0600

    working through runtime issues

--- go.mod
+++ go.mod
@@ -3,15 +3,9 @@ module github.com/mhalder-recurly/cloud-function-remove-worker
 go 1.16
 
 require (
-	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
-	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
-	github.com/spf13/viper v1.7.1 // indirect
-	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
-	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
-	google.golang.org/api v0.13.0 // indirect
-	k8s.io/api v0.18.1
-	k8s.io/apimachinery v0.18.1
-	k8s.io/client-go v1.5.2
+	github.com/spf13/viper v1.7.1
+	google.golang.org/api v0.13.0
+	k8s.io/client-go v0.0.0-00010101000000-000000000000
 )
 
 replace (
--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"log"
-	"encoding/base64"
-	"k8s.io/client-go/kubernetes"
+
 	"github.com/spf13/viper"
 	"google.golang.org/api/container/v1"
-	"k8s.io/api"
-	"k8s.io/apimachinery"
-	"k8s.io/client-go"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
-	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 )
 
 // Message is the payload of a Pub/Sub event.  Initial build is Stackdriver alert -> pub/sub ->
@@ -43,44 +37,41 @@ func main() {
 		log.Fatal("POD_LABEL must be set")
 	}
 
-	var projectID string = viper.Get("PROJECT_ID")
-	var clusterName string = viper.Get("CLUSTER_NAME")
-
 }
 
-func RemovePod(ctx context.Context, prjectId string, labelSelector metav1.LabelSelector ){
-	kubeConfig, err := AcquireContext(ctx, projectId)
-	if err != nil {
-		return err
-	}
+// func RemovePod(ctx context.Context, prjectId string, labelSelector metav1.LabelSelector) {
+// 	kubeConfig, err := AcquireContext(ctx, projectId)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	for clusterName := range kubeConfig.Clusters {
-		cfg, err := clientcmd.NewNonInteractiveClientConfig(*kubeConfig, clusterName, &clientcmd.ConfigOverrides{CurrentContext: clusterName}, nil).ClientConfig()
-		if err != nil {
-			return fmt.Errorf("failed to create k8s config: %w", err)
-		}
-		k8s, err := kubernetes.NewConfigFor(ctx)
-		if err != nil {
-			return fmt.Errorf("failed to init k8s client for cluster: %w", err)
-		}
+// 	for clusterName := range kubeConfig.Clusters {
+// 		cfg, err := clientcmd.NewNonInteractiveClientConfig(*kubeConfig, clusterName, &clientcmd.ConfigOverrides{CurrentContext: clusterName}, nil).ClientConfig()
+// 		if err != nil {
+// 			return fmt.Errorf("failed to create k8s config: %w", err)
+// 		}
+// 		k8s, err := kubernetes.NewConfigFor(ctx)
+// 		if err != nil {
+// 			return fmt.Errorf("failed to init k8s client for cluster: %w", err)
+// 		}
 
-		// podList, err := k8s.CoreV1().Pods.List(metav1.ListOptions{})
-		ns, err := k8s.CoreV1().Namespaces().List(metav1.ListOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to list namespaces cluster=%s: %w", clusterName, err)
-		}
+// 		// podList, err := k8s.CoreV1().Pods.List(metav1.ListOptions{})
+// 		ns, err := k8s.CoreV1().Namespaces().List(metav1.ListOptions{})
+// 		if err != nil {
+// 			return fmt.Errorf("failed to list namespaces cluster=%s: %w", clusterName, err)
+// 		}
 
-		log.Printf("Namespaces found in cluster=%s", clusterName)
+// 		log.Printf("Namespaces found in cluster=%s", clusterName)
 
-		for _, item := range ns.Items {
-			log.Println(item.Name)
-		}
+// 		for _, item := range ns.Items {
+// 			log.Println(item.Name)
+// 		}
 
-	}
-}
+// 	}
+// }
 
 // AcquireContext is used to setup GKE context and allow interacting with the clusters apis
-func AcquireContext(ctx context.Context, projectID string) (*api.Config, error) {
+func AcquireContext(ctx context.Context, projectId string, zone string, clusterId string) (*api.Config, error) {
 	svc, err := container.NewService(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("container.NewService: %w", err)
@@ -94,26 +85,27 @@ func AcquireContext(ctx context.Context, projectID string) (*api.Config, error) 
 		Contexts:   map[string]*api.Context{},  //ref name to context config
 	}
 
-
-	// Get the Cluster based on the CLUSTER_NAME variable being passed
-	resp, err := svc.Projects.Zones.Clusters.Get(projectId string, zone string, clusterId string //call via CLUSTER_NAME var for now)
+	resp, err := svc.Projects.Zones.Clusters.List(projectId, "-").Context(ctx).Do()
 	if err != nil {
-		return nil, fmt.Errorf("cluster get error project %s %w", prjectId, err)
+		return nil, fmt.Errorf("clusters list project=%s: %w", projectId, err)
+	}
+
+	for i, p := range resp.Clusters {
+		return nil, fmt.Errorf("my msg %s %s", i, p)
 	}
 
 	for _, f := range resp.Clusters {
 		name := fmt.Sprintf(f.Name)
-		location := fmt.Sprintf(f.Location)
-		cert, err := base64.StdEncoding.DecodeString(f.masterAuth.clusterCaCertificate)
+		cert, err := base64.StdEncoding.DecodeString(f.MasterAuth.ClusterCaCertificate)
 		if err != nil {
-			return nil, fmt.Errorf("invalid cert for cluster=%s cert=%s: %w" name, f.masterAuth.clusterCaCertificate, err)
+			return nil, fmt.Errorf("invalid cert for cert=%s: %w", f.MasterAuth.ClusterCaCertificate, err)
 		}
 		ret.Clusters[name] = &api.Cluster{
 			CertificateAuthorityData: cert,
-			Server: "https://" + f.Endpoint,
+			Server:                   "https://" + f.Endpoint,
 		}
 		ret.Contexts[name] = &api.Contexts{
-			Cluster: name,
+			Cluster:  name,
 			AuthInfo: name,
 		}
 		ret.AuthInfos[name] = &api.AuthInfo{
@@ -121,8 +113,9 @@ func AcquireContext(ctx context.Context, projectID string) (*api.Config, error) 
 				Name: "gcp",
 				Config: map[string]string{
 					"scopes": "https://www.googleapis.com/auth/cloud-platform",
-				}
-			}
+				},
+			},
 		}
 	}
+	return &ret, nil
 }
